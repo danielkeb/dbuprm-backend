28886ec0ffc5d509f3f2503212d3895cb588dc08
danielkeb <danielkebede381@gmail.com>
2024-08-29 08:36:29 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-  await app.listen(3333);
+  
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
 }
 bootstrap();
